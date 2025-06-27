f0e2a9312f9337636816227a1fb466b824967740
SriHariPrasathR <118532333+SrihariPrasathR@users.noreply.github.com>
2025-06-27 13:53:25 +0530

    Add files via upload

---
+++ Printing_UsingStructures.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+struct details{
+    char b[20];
+    int a;
+};
+int main(){
+   details s1={"Hello",16};
+   cout<<s1.b;
+
+}
---
+++ Structures.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+struct details{
+    char b[20];
+    int a;
+};
+int main(){
+  cout<<sizeof(details);  
+}
